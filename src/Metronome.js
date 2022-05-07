2163fbee674758cfa34c3127f6b28536567d8064
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-05-07 00:52:56 -0400

    created jsx for basic metronome

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Metronome = () => {
+  let bpm = 100;
+  let playing = false;
 
-  return(
-    <div>
-      hi
+    return(
+    <div className="metronome">
+      <div className="bpm-slider">
+        <div>{bpm} BPM</div>
+        <input type="range" min="20" max="500" value={bpm} />
+      </div>
+      <button>{playing ? 'Stop': 'Start'}</button>
     </div>
   )
 }
